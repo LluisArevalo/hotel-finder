37f2f1343ceed3c119f0b149311d2312005def85
LluisArevalo <thorin119@gmail.com>
2015-12-12 10:16:40 +0100

    Change the routes. Now I'm using convention over configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'site#home'
 
-  # scope '/api' do
-    #resources :hotels, only: [:create]
-    # resources :point_of_interests, only: [:create]
-  # end
-  
-  get '/api/hotels' => 'hotels#create'
-  post '/api/point_of_interests', :to => 'point_of_interests#create'
+  scope '/api' do
+    resources :hotels, only: [:create]
+    resources :point_of_interests, only: [:create]
+  end
+
   get '/api/point_of_interests/get/:name' => 'point_of_interests#search'
   get '/api/point_of_interests/get_hotels_around/' => 'point_of_interests#find_hotels_around'
 end
