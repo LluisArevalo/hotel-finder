de446446bc5df58542679e24c5732b2245832b77
LluisArevalo <thorin119@gmail.com>
2015-12-15 15:15:42 +0100

    Add the relation between hotels and cities

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,4 @@
 class Hotel < ActiveRecord::Base
+  belongs_to :city
   validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215120001) do
+ActiveRecord::Schema.define(version: 20151215140924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151215120001) do
     t.datetime  "updated_at",                                                          null: false
     t.float     "price"
     t.string    "website"
+    t.integer   "city_id"
   end
 
   create_table "point_of_interests", force: :cascade do |t|
